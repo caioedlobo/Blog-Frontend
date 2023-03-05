a931adc0b5a2af0c55781e5f679b61b5acc6c5c0
Caio Lobo <caioeduardolobo@gmail.com>
2023-03-05 17:53:57 -0300

    create Dialog to create post

--- src/components/pages/Feed.js
+++ src/components/pages/Feed.js
@@ -9,6 +9,7 @@ import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
 import Button from "@mui/material/Button";
+import { TextField } from "@mui/material";
 
 const posts = [
   {
@@ -32,6 +33,9 @@ const Feed = () => {
   // eslint-disable-next-line
   const [searchText, setSearchText] = useState("");
   const [filteredPosts, setFilteredPosts] = useState(posts);
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
 
   const handleSearchTextChange = (event) => {
     const text = event.target.value.toLowerCase();
@@ -44,6 +48,36 @@ const Feed = () => {
         post.name.toLowerCase().includes(text)
     );
     setFilteredPosts(filtered);
+  };
+
+  const handleOpenForm = () => {
+    setIsFormOpen(true);
+  };
+
+  const handleCloseForm = () => {
+    setIsFormOpen(false);
+  };
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleSave = () => {
+    const newPost = {
+      title: title,
+      text: text,
+      name: "Seu nome",
+    };
+
+    const newPosts = [...posts, newPost];
+    setFilteredPosts(newPosts);
+    setTitle("");
+    setText("");
+    handleCloseForm();
   };
 
   return (
@@ -59,6 +93,45 @@ const Feed = () => {
           name={post.name}
         />
       ))}
+
+      <Fab
+        color="white"
+        style={{
+          position: "fixed",
+          bottom: "15px",
+          right: "15px",
+        }}
+        onClick={handleOpenForm}
+      >
+        <AddIcon />
+      </Fab>
+
+      <Dialog open={isFormOpen} onClose={handleCloseForm}>
+        <DialogTitle>Novo Post</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="title"
+            label="Título"
+            type="text"
+            fullWidth
+          />
+          <TextField
+            margin="dense"
+            id="text"
+            label="Corpo do Texto"
+            type="text"
+            rows={5}
+            fullWidth
+            multiline
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseForm}>Cancelar</Button>
+          <Button onClick={handleCloseForm}>Salvar</Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 };
